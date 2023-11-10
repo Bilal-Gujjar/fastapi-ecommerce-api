c01d614458d0f52248678ab7d076870bda24485f
Bilal-Gujjar <bilaltahir515@gmail.com>
2023-11-10 17:40:31 +0500

    id increment bug-fix

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,14 @@
 # app/models/user.py
-from sqlalchemy import Column, Integer, String, Sequence, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
 from app.core.database import Base
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    id = Column(Integer, primary_key=True)  # Auto-incremented by PostgreSQL
     username = Column(String(50), unique=True)
     email = Column(String(100), unique=True)
     hashed_password = Column(String(100))
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    # Add relationships and any additional columns here
