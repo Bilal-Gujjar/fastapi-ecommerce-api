7f2b2113bbf444d98f6bc1606bbf65a49572501c
Bilal-Gujjar <bilaltahir515@gmail.com>
2023-11-12 14:45:21 +0500

    updated

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/sales")
+@router.get("/users")
 def get_sales_data():
     # Placeholder. This should return sales data.
     return {"message": "Sales data"}
