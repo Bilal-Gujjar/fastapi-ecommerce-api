7fe0bb14293b5c55b5db04a5546ab5c21a4df871
Bilal-Gujjar <bilaltahir515@gmail.com>
2023-11-15 17:32:26 +0500

    add userID in DB

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,16 @@
 # app/models/user.py
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID  # Import this for UUID
+import uuid  # This is for generating UUID values
 from datetime import datetime
 from app.core.database import Base
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)  # Auto-incremented by PostgreSQL
+    id = Column(Integer, primary_key=True)
+    userId= Column(UUID(as_uuid=True), unique=True, default=uuid.uuid4)
     username = Column(String(50), unique=True)
     email = Column(String(100), unique=True)
     hashed_password = Column(String(100))
     created_at = Column(DateTime, default=datetime.utcnow)
-
-    # Add relationships and any additional columns here
+    userId= Column(UUID(as_uuid=True), unique=True, default=uuid.uuid4)
